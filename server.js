39e7a6810fc63625ee8aea278fd433e4f91107b8
Fikiresilase <noahkelemework@gmail.com>
2025-08-19 12:22:49 +0300

    cors

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ dotenv.config();
 const app = express();
 
 // Enable CORS for all origins, no credentials
-app.use(cors({ origin: "http://localhost:5173", credentials: false }));
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    
+}));
 app.use(bodyParser.json());
 
 // Configurable constants
